e140add2530e2d2aac4f59ef200b4323276cb2c2
Kunlun-Hub <Kunlun-Hub@users.noreply.github.com>
2026-08-31 06:19:10 +0000

    修改站点配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -11,18 +11,18 @@ const pages = resolvePageToggles({
 	// ── 社交 (Social) ──────────────────────────────────
 
 	// 友链页面开关
-	friends: true,
+	friends: false,
 	// 留言板页面开关，需要配置评论系统
-	guestbook: true,
+	guestbook: false,
 
 	// ── 我的 (My) ──────────────────────────────────
 
 	// 动态页面开关
-	dynamic: true,
+	dynamic: false,
 	// 相册页面开关
-	gallery: true,
+	gallery: false,
 	// 书签导航页面开关
-	booknav: true,
+	booknav: false,
 	// 哔哩哔哩追番页面开关
 	bilibili: false,
 	// 番组计划页面开关
@@ -40,24 +40,23 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "四维博客",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Four-Dimensional Blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://fire.4w.ink",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"四维博客-个人技术分享,日常事件记录,故不积跬步,无以至千里,不积小流，无以成江海。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
-		"ACGN",
+		"四维博客",
+        "日常博客",
+        "日常记录",
 		"博客",
 		"技术博客",
 		"静态博客",
@@ -115,7 +114,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "四维博客",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
@@ -127,7 +126,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-09-01",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -324,6 +323,8 @@ export const siteConfig: SiteConfig = {
 			"*.bilibili.com",
 			"*.myanimelist.net",
 			"*.vndb.org",
+            "*.4w.ink",
+            "*.01012388.xyz",
 		],
 	},
 
